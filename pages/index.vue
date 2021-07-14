cb64140a82f5028f62af127892523fd5c585504a
Adrián Carmona <acarmona.dev@gmail.com>
2021-07-14 21:30:36 +0200

    init footer

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,53 @@
 <template>
-  <div class="bg-white text-black"><UINavbar />INDEX</div>
+  <div class="bg-white text-black">
+    <UINavbar />
+    <section>
+      <div class="container px-5 py-24 mx-auto">
+        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto">
+          <p>
+            Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
+            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
+            taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
+            fanny pack vaporware. Man bun next level coloring book skateboard
+            four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
+            indigo juice before they sold out gentrify plaid gastropub normcore
+            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
+            shoindigoitch ethical authentic, yr scenester sriracha forage
+            franzen organic drinking vinegar.
+          </p>
+          <p>
+            Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
+            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
+            taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
+            fanny pack vaporware. Man bun next level coloring book skateboard
+            four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
+            indigo juice before they sold out gentrify plaid gastropub normcore
+            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
+            shoindigoitch ethical authentic, yr scenester sriracha forage
+            franzen organic drinking vinegar.
+          </p>
+          <p>
+            Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
+            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
+            taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
+            fanny pack vaporware. Man bun next level coloring book skateboard
+            four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
+            indigo juice before they sold out gentrify plaid gastropub normcore
+            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
+            shoindigoitch ethical authentic, yr scenester sriracha forage
+            franzen organic drinking vinegar.
+          </p>
+        </div>
+      </div>
+    </section>
+    <UIMainFooter />
+  </div>
 </template>
 
-<script>
-</script>
+><style>
+::-webkit-scrollbar {
+  height: 4px; /* height of horizontal scrollbar ← You're missing this */
+  width: 4px; /* width of vertical scrollbar */
+  border: 2px solid #b2ebf9;
+}
+</style>
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="bg-white text-black">
     <UINavbar />
-    <section class="text-gray-600 body-font">
-      <div class="container lg:px-80 px-5 py-32 mx-auto">
-        <div class="flex flex-wrap w-full mb-20">
-          <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
-            <h1
-              class="
-                sm:text-3xl
-                text-2xl
-                font-medium
-                title-font
-                mb-2
-                text-gray-900
-              "
-            >
-              Pitchfork Kickstarter Taxidermy
-            </h1>
-            <div class="h-1 w-20 bg-blue-500 rounded"></div>
-          </div>
+
+    <section>
+      <div class="container px-5 py-24 mx-auto">
+        <div class="xl:w-1/2 lg:w-3/4 w-full mx-auto">
+          <p>
+            Edison bulb retro cloud bread echo park, helvetica stumptown taiyaki
+            taxidermy 90's cronut +1 kinfolk. Single-origin coffee ennui shaman
+            taiyaki vape DIY tote bag drinking vinegar cronut adaptogen squid
+            fanny pack vaporware. Man bun next level coloring book skateboard
+            four loko knausgaard. Kitsch keffiyeh master cleanse direct trade
+            indigo juice before they sold out gentrify plaid gastropub normcore
+            XOXO 90's pickled cindigo jean shorts. Slow-carb next level
+            shoindigoitch ethical authentic, yr scenester sriracha forage
+            franzen organic drinking vinegar.
+          </p>
         </div>
       </div>
     </section>
